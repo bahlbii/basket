eea97ddc7981a4540b3779723fa64a27707ce3b8
bahlbii <bahlbibg@gmail.com>
2022-12-24 00:24:51 +0300

    home & cart routes work

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from "@angular/platform-browser";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./header/header.component";
+import { HomeComponent } from './home/home.component';
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
 	declarations: [
 		AppComponent,
-		HeaderComponent
+		HeaderComponent,
+  HomeComponent,
+  CartComponent
 	],
 	imports: [
 		BrowserModule,
