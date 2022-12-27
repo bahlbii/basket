560af106277098b9773eb35ce5879a103defa81d
bahlbii <bahlbibg@gmail.com>
2022-12-27 10:06:15 +0300

    cart compnonent done

--- client/src/app/cart/cart.component.ts
+++ client/src/app/cart/cart.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { faLessThan, faMinusCircle, faPlusCircle } from "@fortawesome/free-solid-svg-icons";
+import { cart, order } from "../listingData";
+import { ListingService } from "../services/listing.service";
 
 @Component({
     selector: "app-cart",
@@ -6,12 +10,73 @@ import { Component, OnInit } from "@angular/core";
     styleUrls: ["./cart.component.scss"]
 })
 export class CartComponent implements OnInit {
+    //plus minus arrow icons
+    iconArrow = faLessThan;
+    iconPlus = faPlusCircle;
+    iconMinus = faMinusCircle;
+    cartData: cart[] | undefined;
 
-    // eslint-disable-next-line @typescript-eslint/no-empty-function
-    constructor() { }
+    constructor(private product: ListingService, private router: Router) { }
 
-    // eslint-disable-next-line @typescript-eslint/no-empty-function
     ngOnInit(): void {
+        this.product.currentCart().subscribe((result) => {
+
+            // let price = 0;
+            this.cartData = result;
+            // result.forEach((item) => {
+            //     if (item.quantity) {
+            //         price = price + (+item.price * +item.quantity);
+            //     }
+            // });
+            // this.totalPrice = price + (price / 10) + 100 - (price / 10);
+
+            // console.warn(this.totalPrice);
+
+        });
     }
 
+    removeItem(cartNumber: number | undefined) {
+        cartNumber && this.cartData && this.product.deleteFromCart(cartNumber)
+            .subscribe(() => {
+                this.getCart();
+            });
+    }
+
+    getCart() {
+        this.product.currentCart().subscribe((result) => {
+            this.cartData = result;
+        });
+    }
+    decItemQuantity(itemId: any) { //toFix
+
+        this.product.currentCart().subscribe((result) => {
+            this.cartData = result; //this is all of the cart
+
+            const itemToUpdate = this.cartData.findIndex((item => item.productId == itemId));
+            console.warn(`objIndex: ${itemToUpdate}`);
+
+            //update quantity
+            this.cartData[itemToUpdate].quantity = 10; //toFix
+            console.warn(this.cartData[itemToUpdate]);
+        });
+    }
+    incItemQuantity() {
+        console.warn("q increased");
+    }
+    submitOrder() {
+       
+        if (this.cartData) {
+            this.cartData?.forEach((item) => {
+                setTimeout(async () => {
+                    item.id && this.product.emptyFullCart(item.id);
+                }, 700);
+            });
+            console.warn("cart emptied");
+        }
+
+        this.router.navigate(["/"]);
+    }
+    continueShopping() {
+        this.router.navigate(["/"]);
+    }
 }
