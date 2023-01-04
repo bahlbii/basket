dc1e71c652a93978d5e7d82ab06314975eb11aea
bahlbii <bahlbibg@gmail.com>
2023-01-04 20:13:20 +0300

    sticky mobile nav ✅

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, HostListener, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { faCartPlus } from "@fortawesome/free-solid-svg-icons";
+import { faKey } from "@fortawesome/free-solid-svg-icons";
+import { faHome } from "@fortawesome/free-solid-svg-icons";
 import { product } from "../listingData";
 import { ListingService } from "../services/listing.service";
 
@@ -10,24 +13,43 @@ import { ListingService } from "../services/listing.service";
     styleUrls: ["./header.component.scss"],
 })
 export class HeaderComponent implements OnInit {
-    icon = faBars;
+
+    iconBars = faBars;
+    iconHome = faHome;
+    iconCart = faCartPlus;
+    iconLogout = faKey;
+
     isLoggedIn = "default";
     userName = "";
     cartItems = 0;
-    cartData:product|undefined;
+    cartData: product | undefined;
     searchResult: undefined | product[];
 
-    constructor(private route: Router, private product: ListingService) {}
+    //toFix
+    isMenuOpened = true;
+
+    navbarfixed = false;
+
+    @HostListener("window:scroll", ["$event"]) onscroll() {
+        if (window.scrollY > 66) {
+            this.navbarfixed = true;
+        }
+        else {
+            this.navbarfixed = false;
+        }
+    }
+
+    constructor(private route: Router, private product: ListingService) { }
 
     ngOnInit(): void {
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
         this.route.events.subscribe((val: any) => {
             if (val.url) {
-                if(localStorage.getItem("user")){
+                if (localStorage.getItem("user")) {
                     const userStore = localStorage.getItem("user");
                     const userInfo = userStore && JSON.parse(userStore);
                     // this.userName = "Signed in: "+userInfo.name;
-                    this.isLoggedIn= "user";
+                    this.isLoggedIn = "user";
                     this.product.getCartList(userInfo.id);
                 }
                 else {
@@ -35,22 +57,22 @@ export class HeaderComponent implements OnInit {
                 }
             }
         });
-        const cartData= localStorage.getItem("localCart");
-        if(cartData){
-            this.cartItems= JSON.parse(cartData).length;
+        const cartData = localStorage.getItem("localCart");
+        if (cartData) {
+            this.cartItems = JSON.parse(cartData).length;
         }
-        this.product.cartData.subscribe((items)=>{
-            this.cartItems= items.length;
+        this.product.cartData.subscribe((items) => {
+            this.cartItems = items.length;
         });
     }
 
-    signOut(){
+    signOut() {
         localStorage.removeItem("user");
         this.route.navigate(["/user"]);
         this.userName = "";
         this.product.cartData.emit([]);
     }
-      
+
     findProduct(query: KeyboardEvent) {
         if (query) {
             const element = query.target as HTMLInputElement;
@@ -62,12 +84,12 @@ export class HeaderComponent implements OnInit {
             });
         }
     }
-    showMobileMenu(){
-        console.warn("mobile menu visible");
-        
+    showMobileMenu() {
+        this.isMenuOpened = !this.isMenuOpened;
+        console.log(this.isMenuOpened);
     }
 
-    submitSearch(val: string){
+    submitSearch(val: string) {
         console.warn(`query: ${val}`);
         this.route.navigate([`search/${val}`]);
     }
